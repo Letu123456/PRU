c28fe47f28fc75fd4d63f1972a958b097ae6affe
Letu123456 <anhtu200003@gmail.com>
2024-06-04 15:37:56 +0700

    PRU

---
+++ DIDemo/DIDemo/Model/Book.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DIDemo.Model
+{
+    class Book : IBook
+    {
+        public string Title { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string Author { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public double Price { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    }
+}
---
+++ DIDemo/DIDemo/Utilities/Utilities.cs
@@ -0,0 +1,28 @@
+﻿using DIDemo.Model;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+internal class Utilities
+{
+    static string ReadFile(string filename)
+    {
+        return File.ReadAllText(filename);
+    }
+    internal static List<Book> ReadData()
+    {
+        var cadJSON = ReadFile("Data/BookStore.json");
+        return JsonConvert.DeserializeObject<List<Book>>(cadJSON);
+
+    }
+
+    internal static List<Book> ReadDataExtra()
+    {
+        List<Book> books = ReadData();
+        var cadJSON = ReadFile("Data/BookStore1.json");
+        books.AddRange(JsonConvert.DeserializeObject<List<Book>>(cadJSON));
+        return books;
+    }
+
+}
+
+
